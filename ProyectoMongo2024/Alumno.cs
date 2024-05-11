186d63df27fc1d022ed1eb32fb303083f724e8bf
ElConra <qcconrado@gmail.com>
2024-05-11 12:34:15 -0700

    Agregando Proyecto

---
+++ ProyectoMongo2024/Alumno.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMongo2024
+{
+    internal class Alumno
+    {
+        
+        private readonly IMongoCollection<Alumno> _collection;
+
+        public Alumno()
+        { 
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase("Escuela");
+            _collection = database.GetCollection<Alumno>("alumnos");
+
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+
+
+        [BsonElement("apellido")]
+        public string Apellido { get; set; }
+
+
+        [BsonElement("matricula")]
+        public string Matricula { get; set; }
+
+
+        [BsonElement("grupo")]
+        public string Grupo { get; set; }
+
+        public void Agregar(Alumno alumno)
+        {
+            _collection.InsertOne(alumno);
+        }
+    }
+}
---
+++ ProyectoMongo2024/Program.cs
@@ -0,0 +1,11 @@
+﻿using ProyectoMongo2024;
+
+Alumno alumno = new Alumno();
+alumno.Nombre = "Conrado";
+alumno.Apellido = "Quezada";
+alumno.Matricula = "159357";
+alumno.Grupo = "scomm61";
+
+Console.WriteLine("Insertando un elemento");
+alumno.Agregar(alumno);
+Console.WriteLine("Elemento agregado");
